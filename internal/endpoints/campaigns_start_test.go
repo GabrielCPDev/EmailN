8318aab036c887dede67710d4fa68b23e4dfd0c1
GabrielCPDev <gabrielcpcontato@outlook.com>
2024-09-23 13:09:16 -0300

    refatorando testes do pacote de endpoint

--- internal/endpoints/campaigns_start_test.go
+++ internal/endpoints/campaigns_start_test.go
@@ -1,41 +1,35 @@
 package endpoints
 
 import (
-	internalmock "emailn/internal/test/internalMock"
 	"errors"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-func Test_CampaignsStart_should_return_200(t *testing.T) {
-	assert := assert.New(t)
-	service := new(internalmock.CampaignServiceMock)
-	service.On("Start", mock.Anything).Return(nil)
-	handler := Handler{CampaignService: service}
-	req, _ := http.NewRequest("PATH", "/", nil)
-	rr := httptest.NewRecorder()
+func Test_CampaignsStart_200(t *testing.T) {
+	setup()
+	campaignId := "xpto"
+	service.On("Start", mock.MatchedBy(func(id string) bool {
+		return id == campaignId
+	})).Return(nil)
+	req, rr := newHttpTest("PATCH", "/", nil)
+	req = addParameter(req, "id", campaignId)
 
 	_, status, err := handler.CampaignStart(rr, req)
 
-	assert.Nil(err)
-	assert.Equal(200, status)
+	assert.Equal(t, 200, status)
+	assert.Nil(t, err)
 }
 
-
-func Test_CampaignsStart_should_return_err(t *testing.T) {
-	assert := assert.New(t)
-	service := new(internalmock.CampaignServiceMock)
-	errExpected := errors.New("something is whrong")
+func Test_CampaignsStart_Err(t *testing.T) {
+	setup()
+	errExpected := errors.New("something wrong")
 	service.On("Start", mock.Anything).Return(errExpected)
-	handler := Handler{CampaignService: service}
-	req, _ := http.NewRequest("PATH", "/", nil)
-	rr := httptest.NewRecorder()
+	req, rr := newHttpTest("PATCH", "/", nil)
 
 	_, _, err := handler.CampaignStart(rr, req)
 
-	assert.Equal(errExpected, err)
+	assert.Equal(t, errExpected, err)
 }
